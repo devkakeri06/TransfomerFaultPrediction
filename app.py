5ffba0656fd62eda90a3ae3a73276d6efa423388
Devaansh Kakeri <devaansh6@gmail.com>
2023-10-21 16:38:56 +0530

    Add files via upload

--- app.py
+++ app.py
@@ -1,55 +1,20 @@
-
-
-# from flask import Flask, request
 import numpy as np
 import pickle
 import pandas as pd
-# import flasgger
-# from flasgger import Swagger
 import streamlit as st
 
-# app=Flask(__name__)
-# Swagger(app)
+pickle_in1 = open("logistic_regression_model.pkl","rb")
+lr_classifier=pickle.load(pickle_in1)
 
-pickle_in = open("logistic_regression_model.pkl","rb")
-classifier=pickle.load(pickle_in)
+pickle_in2 = open("svc_model.pkl", "rb")
+svc_classifier = pickle.load(pickle_in2)
 
-# @app.route('/')
-def welcome():
-    return "Welcome All"
+pickle_in3 = open("knn_model.pkl", "rb")
+knn_classifier = pickle.load(pickle_in3)
 
-# @app.route('/predict',methods=["Get"])
-def predict_note_authentication(OTI,	WTI,	ATI,	OLI,	OTI_A,	OTI_T, VL1,	VL2,	VL3,	IL1,	IL2,	IL3,	VL12,	VL23,	VL31,	INUT):
 
-    """Let's Authenticate the Banks Note
-    This is using docstrings for specifications.
-    ---
-    parameters:
-      - name: variance
-        in: query
-        type: number
-        required: true
-      - name: skewness
-        in: query
-        type: number
-        required: true
-      - name: curtosis
-        in: query
-        type: number
-        required: true
-      - name: entropy
-        in: query
-        type: number
-        required: true
-    responses:
-        200:
-            description: The output values
+def predict_probability(OTI, WTI, ATI, OLI, OTI_A, OTI_T, VL1, VL2, VL3, IL1, IL2, IL3, VL12, VL23,	VL31, INUT):
 
-    """
-    # variance=request.args.get("variance")
-    # skewness=request.args.get("skewness")
-    # curtosis=request.args.get("curtosis")
-    # entropy=request.args.get("entropy")
     OTI = float(OTI)
     WTI = float(WTI)
     ATI = float(ATI)
@@ -67,55 +32,71 @@ def predict_note_authentication(OTI,	WTI,	ATI,	OLI,	OTI_A,	OTI_T, VL1,	VL2,	VL3,
     VL31 = float(VL31)
     INUT = float(INUT)
 
-    prediction=classifier.predict([[OTI,	WTI,	ATI,	OLI,	OTI_A,	OTI_T, VL1,	VL2,	VL3,	IL1,	IL2,	IL3,	VL12,	VL23,	VL31,	INUT]])
-    prediction = prediction[0]
+    lr_pred = lr_classifier.predict([[OTI,	WTI,	ATI,	OLI,	OTI_A,	OTI_T, VL1,	VL2,	VL3,	IL1,	IL2,	IL3,	VL12,	VL23,	VL31,	INUT]])
+    lr_pred = lr_pred[0]
+
+    svc_pred = svc_classifier.predict([[OTI,	WTI,	ATI,	OLI,	OTI_A,	OTI_T, VL1,	VL2,	VL3,	IL1,	IL2,	IL3,	VL12,	VL23,	VL31,	INUT]])
+    svc_pred = svc_pred[0]
+
+    knn_pred = knn_classifier.predict([[OTI,	WTI,	ATI,	OLI,	OTI_A,	OTI_T, VL1,	VL2,	VL3,	IL1,	IL2,	IL3,	VL12,	VL23,	VL31,	INUT]])
+    knn_pred = knn_pred[0]
+
+    probability = ((lr_pred + svc_pred + knn_pred) / 3.0) * 100.0
+    probability = round(probability, 2)
+
     answer = ""
-    # print(prediction[0])
-    if prediction == 1.0:
-        answer = "Faulty!"
-    elif prediction == 0.0:
-        answer = "Everything looks good!"
+
+    if probability > 67.0:
+        answer = "Very High Risk!"
+    elif probability > 34.0:
+        answer = "Moderate Risk!"
+    elif probability > 1.0:
+        answer = "Low Risk!"
     else:
-        answer = "Unknown result"
+        answer = "No Risk"
 
-    return "Status:-"+answer
+    return "Transformer Fault Probabilty: "+str(probability)+"% "+answer
 
-# @app.route('/predict_file',methods=["POST"])
 def main():
     st.title("Transformer Fault Prediction using Machine Learning")
+
+    st.subheader("Input Parameters for the Transformer")
     html_temp = """
-    <div style="background-color:tomato;padding:10px">
-    <h2 style="color:white;text-align:center;">Input Parameters for the Transformer</h2>
+    <div style="background-color:#ff6347; padding:10px; border-radius:10px;">
+    <h3 style="color:white; text-align:center;">Transformer Parameters</h3>
     </div>
     """
-    st.markdown(html_temp,unsafe_allow_html=True)
-    OTI = st.text_input("OTI", "Type Here")
-    WTI = st.text_input("WTI", "Type Here")
-    ATI = st.text_input("ATI", "Type Here")
-    OLI = st.text_input("OLI", "Type Here")
-    OTI_A = st.text_input("OTI_A", "Type Here")
-    OTI_T = st.text_input("OTI_T", "Type Here")
-    VL1 = st.text_input("VL1", "Type Here")
-    VL2 = st.text_input("VL2", "Type Here")
-    VL3 = st.text_input("VL3", "Type Here")
-    IL1 = st.text_input("IL1", "Type Here")
-    IL2 = st.text_input("IL2", "Type Here")
-    IL3 = st.text_input("IL3", "Type Here")
-    VL12 = st.text_input("VL12", "Type Here")
-    VL23 = st.text_input("VL23", "Type Here")
-    VL31 = st.text_input("VL31", "Type Here")
-    INUT = st.text_input("INUT", "Type Here")
-    prediction=""
-    result=""
+    st.markdown(html_temp, unsafe_allow_html=True)
+
+    col1, col2, col3 = st.columns(3)
+    with col1:
+        OTI = st.text_input("OTI", "Type Here")
+        WTI = st.text_input("WTI", "Type Here")
+        ATI = st.text_input("ATI", "Type Here")
+        OLI = st.text_input("OLI", "Type Here")
+        OTI_A = st.text_input("OTI_A", "Type Here")
+        OTI_T = st.text_input("OTI_T", "Type Here")
+    with col2:
+        VL1 = st.text_input("VL1", "Type Here")
+        VL2 = st.text_input("VL2", "Type Here")
+        VL3 = st.text_input("VL3", "Type Here")
+        IL1 = st.text_input("IL1", "Type Here")
+        IL2 = st.text_input("IL2", "Type Here")
+        IL3 = st.text_input("IL3", "Type Here")
+    with col3:
+        VL12 = st.text_input("VL12", "Type Here")
+        VL23 = st.text_input("VL23", "Type Here")
+        VL31 = st.text_input("VL31", "Type Here")
+        INUT = st.text_input("INUT", "Type Here")
+
+    st.write('<style>div.stButton > button {background-color: #4CAF50; color: white; font-size: 16px;}</style>', unsafe_allow_html=True)
+
     if st.button("Predict"):
-        result=predict_note_authentication(OTI,	WTI,	ATI,	OLI,	OTI_A,	OTI_T, VL1,	VL2,	VL3,	IL1,	IL2,	IL3,	VL12,	VL23,	VL31,	INUT)
+        result = predict_probability(OTI, WTI, ATI, OLI, OTI_A, OTI_T, VL1, VL2, VL3, IL1, IL2, IL3, VL12, VL23, VL31, INUT)
+
+    st.success(f'{result}')
 
 
-    st.success(f'The output is {result}')
-    # st.success('The output is {}'.format(result))
-    if st.button("About"):
-        st.text("Lets LEarn")
-        st.text("Built with Streamlit")
 
 if __name__=='__main__':
     main()
